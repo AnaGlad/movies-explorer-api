7bfbe468624d168ff5bd5a984736d2fa5c3841f0
Anastasia Gladkikh <avpspb117@gmail.com>
2023-08-21 12:31:14 +0300

    validation fix+readme

--- validation/updateUserValidation.js
+++ validation/updateUserValidation.js
@@ -2,8 +2,8 @@ const { celebrate, Joi } = require('celebrate');
 
 const updateUserValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    email: Joi.string().required().pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/),
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/),
   }),
 });
 
